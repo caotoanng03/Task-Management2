30364703a23ab9c9117dafcb52ed75dd0aadd8bb
CaoToan <caotoan940@gmail.com>
2023-11-28 03:16:28 +0100

    Lấy ra chi tiết 1 công việc

--- index.ts
+++ index.ts
@@ -10,13 +10,31 @@ database.connect();
 const app: Express = express();
 const port: number | string = process.env.PORT || 1000;
 
-app.use("/tasks", async (req: Request, res: Response) => {
+app.get("/tasks", async (req: Request, res: Response) => {
     const tasks = await Task.find({ deleted: false });
     res.json({
         code: 200,
-        message: "Success",
         data: tasks
     })
+})
+
+app.get("/tasks/:id", async (req: Request, res: Response): Promise<void> => {
+    const id = req.params.id;
+    try {
+        const task = await Task.findOne({
+            _id: id,
+            deleted: false
+        })
+        res.json({
+            code: 200,
+            data: task
+        })
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: "None existed id"
+        })
+    }
 })
 
 
