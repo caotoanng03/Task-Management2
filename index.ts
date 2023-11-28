3d9aa3bb6248cf96e27a0c5c6466d0b6cf829055
CaoToan <caotoan940@gmail.com>
2023-11-28 06:30:34 +0100

    Cài đăt CORS

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import express, {Express} from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import * as database from "./config/database";
 import mainV1Routes from "./api/v1/routes/index.route";
@@ -9,6 +10,14 @@ database.connect();
 
 const app: Express = express();
 const port: number | string = process.env.PORT || 1000;
+
+// const corsOptions = {
+//   origin: 'http://localhost:3100',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+// app.use(cors(corsOptions));
+
+app.use(cors());
 
 // dùng này thì không cần dùng body-parser khi gửi bằng phương thức patch 
 app.use(express.json());
