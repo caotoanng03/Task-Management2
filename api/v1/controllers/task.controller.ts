7dcfb504a6fef6176c06f571cc42f9571e1c4423
CaoToan <caotoan940@gmail.com>
2023-11-28 04:11:53 +0100

    Sắp xếp theo tiêu chí

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -3,7 +3,7 @@ import {Request, Response} from "express";
 import Task from "../models/task.model";
 
 export const index = async (req: Request, res: Response): Promise<void> => {
-
+    // Find
     interface Find {
         deleted: boolean,
         status?: string
@@ -16,9 +16,19 @@ export const index = async (req: Request, res: Response): Promise<void> => {
     if(req.query.status) {
         find.status = req.query.status.toString();
     }
+    // End Find
 
-    const tasks = await Task.find(find);
-    
+    // Sort
+    const sort = {};
+
+    if(req.query.sortKey && req.query.sortValue) {
+        const sortKey = req.query.sortKey.toLocaleString();
+        sort[sortKey] = req.query.sortValue;
+    }
+    // End Sort
+
+    const tasks = await Task.find(find).sort(sort);
+
     res.json({
         code: 200,
         data: tasks
